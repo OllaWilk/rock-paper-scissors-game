024162fbb1d1570d7faa4247799da618add5d4fd
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2021-07-23 16:15:03 +0200

    read computer move and player move

--- js/script.js
+++ js/script.js
@@ -1,4 +1,58 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+/* Random number from 1-3 */
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+/* Computer move */
+
+let computerMove = '';
+
+if (randomNumber === 1) {
+
+    computerMove = 'rock';
+
+} else if (randomNumber === 2) {
+
+    computerMove = 'paper';
+
+} else {
+
+    computerMove = 'scissors';
+}
+
+console.log('ruch komputera ' + computerMove);
+
+printMessage(`My move: ${computerMove}`);
+
+/* Player move */
+let playerMove = Number(prompt('Choose your move ! 1. Rock, 2. Paper, 3. Scissors'));
+
+if (isNaN(playerMove)) {
+
+    printMessage(`It is not a number`);
+
+} else {
+
+    if (playerMove === 1) {
+
+        playerMove = 'rock';
+
+    } else if (playerMove === 2) {
+
+        playerMove = 'paper';
+
+    } else if (playerMove === 3) {
+
+        playerMove = 'scissors';
+
+    } else if (playerMove >= 3) {
+
+        alert('Choose number from 1 to 3');
+    }
+
+    console.log('ruch gracza ' + playerMove);
+    printMessage(`Youre move is: ${playerMove}`);
+
+}
+
